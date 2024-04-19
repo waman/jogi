2b231895062fd9d8c6704ba22c6b2a1065668eb1
waman <waman10da@gmail.com>
2024-04-20 04:21:44 +0900

    Add some utility methods and functions

--- src/unit/length.rs
+++ src/unit/length.rs
@@ -4,7 +4,16 @@ pub fn length<'a>(value: f64, unit: &'a dyn LengthUnit<'a>) -> Box<dyn Length<'a
     return Box::new(SimpleLinearQuantity{ _value: value, _unit: unit });
 }
 
+
+
 pub trait LengthUnit<'a>: LinearUnit<'a>{}
+
+impl<'a> dyn LengthUnit<'a> + 'a{
+
+    pub fn of(&'a self, value: f64) -> Box<dyn Length<'a> + 'a> {
+        return length(value, self);
+    }
+}
 
 impl<'a> LengthUnit<'a> for SimpleLinearUnit<'a>{}
 
@@ -19,16 +28,21 @@ pub const mm: &dyn LengthUnit = &SimpleLinearUnit{_name: "millimetre", _symbol: 
 #[allow(non_upper_case_globals)]
 pub const km: &dyn LengthUnit = &SimpleLinearUnit{_name: "kilometre", _symbol: "km", _interval: 1000.};
 
+
+
 pub trait Length<'a>: LinearQuantity<'a, dyn LengthUnit<'a> + 'a>{}
 
 impl<'a> Length<'a> for SimpleLinearQuantity<'a, dyn LengthUnit<'a> + 'a>{}
+
+
 
 #[cfg(test)]
 use crate::test_util::f_eq;
 
 #[test]
 fn test_unit_conversion(){
-    let three_cm = length(3., cm);
+    // let three_cm = length(3., cm);
+    let three_cm = cm.of(3.);
 
     assert!(f_eq(three_cm.value_in(cm), 3.));
     assert!(f_eq(three_cm.value_in(m), 0.03));
--- src/unit/time.rs
+++ src/unit/time.rs
@@ -4,7 +4,16 @@ pub fn time<'a>(value: f64, unit: &'a dyn TimeUnit<'a>) -> Box<dyn Time<'a> + 'a
     return Box::new(SimpleLinearQuantity{ _value: value, _unit: unit });
 }
 
+
+
 pub trait TimeUnit<'a>: LinearUnit<'a>{}
+
+impl<'a> dyn TimeUnit<'a> + 'a{
+
+    pub fn of(&'a self, value: f64) -> Box<dyn Time<'a> + 'a> {
+        return time(value, self);
+    }
+}
 
 impl<'a> TimeUnit<'a> for SimpleLinearUnit<'a>{}
 
@@ -17,9 +26,13 @@ pub const h: &dyn TimeUnit = &SimpleLinearUnit{_name: "hour", _symbol: "h", _int
 #[allow(non_upper_case_globals)]
 pub const ms: &dyn TimeUnit = &SimpleLinearUnit{_name: "millisecond", _symbol: "ms", _interval: 0.001};
 
+
+
 pub trait Time<'a>: LinearQuantity<'a, dyn TimeUnit<'a> + 'a>{}
 
 impl<'a> Time<'a> for SimpleLinearQuantity<'a, dyn TimeUnit<'a> + 'a>{}
+
+
 
 
 #[cfg(test)]
@@ -27,7 +40,8 @@ use crate::test_util::f_eq;
 
 #[test]
 fn test_unit_conversion(){
-    let three_ms = time(3., ms);
+    // let three_ms = time(3., ms);
+    let three_ms = ms.of(3.);
 
     assert!(f_eq(three_ms.value_in(ms), 3.));
     assert!(f_eq(three_ms.value_in(s), 0.003));
